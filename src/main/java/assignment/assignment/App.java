4c729102cda6c591630fcbc7d214cc1dfee9d77c
Nirupama <73597479+niruautomation@users.noreply.github.com>
2021-10-15 14:50:35 +0530

    Update App.java

--- src/main/java/assignment/assignment/App.java
+++ src/main/java/assignment/assignment/App.java
@@ -9,5 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        String password = "lfxtest";
+        String token = "vd54fghrthy4y56y6";
+        String apikey = "tirt349875304986-0jfwdotie0";
     }
 }
